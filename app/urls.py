70e374ef79940aa02072dbea49cb0f044309afb7
vincent <ododovincent54@gmail.com>
2019-07-25 15:24:24 +0300

    created a registration folder with login and start-up html

--- app/urls.py
+++ app/urls.py
@@ -19,5 +19,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'',include('instagram.urls'))
+    url(r'',include('instagram.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+]
 ]
