6c4dfdcb93ad02d9dabed119058cc796d02e255d
vincent <ododovincent54@gmail.com>
2019-07-26 15:09:56 +0300

    updated my forms.py file

---
+++ instagram/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from .models import Image, Profile, Comments
+
+
+class SignupForm(UserCreationForm):
+    email = forms.EmailField(max_length=200, help_text = 'Required')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+class ImageForm(forms.ModelForm):
+  
+    class Meta:
+        model = Image
+        exclude = ['likes', 'post_date', 'profile']
+    
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user']
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comments
+        exclude = ['image', 'user']
