c998838c14f1979deff552d62d6870c51b458c44
vincent <ododovincent54@gmail.com>
2019-07-26 14:58:40 +0300

    updated models page and migrated

--- instagram/models.py
+++ instagram/models.py
@@ -1,7 +1,77 @@
 from django.db import models
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
+from pyuploadcare.dj.models import ImageField
 
 # Create your models here.
-class Editor(models.Model):
-    first_name = models.CharField(max_length =30)
-    last_name = models.CharField(max_length =30)
-    email = models.EmailField()
+class Profile(models.Model):
+    # profile_photo = models.ImageField(upload_to = 'profilepics/', default='Image')
+    prof_pic = ImageField(blank=True, manual_crop='800x800')
+    bio = HTMLField()
+    user = models.OneToOneField(User,on_delete=models.CASCADE, primary_key=True)
+
+    def save_profile(self):
+        self.save()
+    
+    @classmethod
+    def search_profile(cls, name):
+        profile = Profile.objects.filter(user__username__icontains = name)
+        return profile
+    
+    @classmethod
+    def get_by_id(cls, id):
+        profile = Profile.objects.get(user = id)
+        return profile
+
+    @classmethod
+    def filter_by_id(cls, id):
+        profile = Profile.objects.filter(user = id).first()
+        return profile
+
+class Image(models.Model):
+    # image_pic = models.ImageField(upload_to = 'p/', default='Image')
+    photo = ImageField(blank=True, manual_crop='800x800')
+    image_name = models.CharField(max_length = 50)
+    image_caption = HTMLField(blank=True)
+    post_date = models.DateTimeField(auto_now=True)
+    likes = models.BooleanField(default=False)
+    profile = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ('-post_date',)
+
+    def save_image(self):
+        self.save()
+    
+    @classmethod
+    def update_caption(cls, update):
+        pass
+    
+    @classmethod
+    def get_image_id(cls, id):
+        image = Image.objects.get(pk=id)
+        return image
+    
+    @classmethod
+    def get_profile_images(cls, profile):
+        images = Image.objects.filter(profile__pk = profile)
+        return images
+    
+    @classmethod
+    def get_all_images(cls):
+        images = Image.objects.all()
+        return images
+
+class Comments(models.Model):
+    comment = HTMLField()
+    posted_on = models.DateTimeField(auto_now=True)
+    image = models.ForeignKey(Image, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def save_comment(self):
+        self.save()
+    
+    @classmethod
+    def get_comments_by_images(cls, id):
+        comments = Comments.objects.filter(image__pk = id)
+        return comments
