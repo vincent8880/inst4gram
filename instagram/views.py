db4bbcbf802469c878bc0bbc4aed16ff08ad6cac
vincent <ododovincent54@gmail.com>
2019-07-26 15:35:32 +0300

    imported all myforms classes to my views.py

--- instagram/views.py
+++ instagram/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from . import views
 from django.contrib.auth.decorators import login_required
 from .models import Image, Profile, Comments
+from .forms import SignupForm, ImageForm, ProfileForm, CommentForm
 
 
 
