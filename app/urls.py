11b4ea697a58ba8ccf3f63ef5715b4d5c107ac2e
vincent <ododovincent54@gmail.com>
2019-07-29 09:23:59 +0300

    added a log out url to my main urls.py7

--- app/urls.py
+++ app/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'',include('instagram.urls')),
     url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^logout/$', views.logout, {"next_page": '/'}
 ]
 
