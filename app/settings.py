08aa0636be0a6c3cd20718ffd5e04a01e8af2ff1
vincent <ododovincent54@gmail.com>
2019-07-29 11:14:24 +0300

    created and updated a gitignore file

--- app/settings.py
+++ app/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'hw#ofqh&6x^rr&rz)g*toi3vd83d4m*idqrw23l^h@^hae-)*#'
+SECRET_KEY = '5566'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
